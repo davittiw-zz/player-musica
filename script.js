14f85bec77eea76e8082b0dc905b3c7343cc3100
Luccas Davi <lucctyte@gmail.com>
2022-01-28 13:09:51 -0300

    musicas js pt 3
    add titulo, artistas e img as músicas no js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+let musicas = {
+    {titulo: 'BSC State Of Mind', artista: 'Squadda', src: 'musicas/BSC State of Mind - Squadda B.mp3', img: 'imagens/1.jpg'}
+
+    {titulo: 'Bay Street Billionaires', artista: 'Squadda', src: 'musicas/Bay Street Billionaires - Squadda B.mp3', img: 'imagens/2.jpg'}
+
+    {titulo: 'Believe', artista: 'NEFFEX', src: 'musicas/Believe - NEFFEX.mp3', img: 'imagens/3.jpg'}
+
+    {titulo: 'Born a Rockstar', artista: 'NEFFEX', src: 'musicas/Born a Rockstar - NEFFEX.mp3', img:'imagens/4.jpg'}
+
+    {titulo: 'Space Age Hustle', artista: 'Squadda', src: 'musicas/Space Age Hustle - Squadda B.mp3', img:'imagens/5.jpg'}
+
+    {titulo: 'Squadda Bambino Tumblr', artista: 'Squadda', src: 'musicas/Squadda Bambino Tumblr - Squadda B.mp3', img:'imagens/6.jpg'}
+
+    {titulo: 'Travel Nonstop', artista: 'Squadda', src: 'musicas/Travel Nonstop - Squadda B.mp3', img:'imagens/7.jpg'}
+};
+
 let musica = document.querySelector('audio');
 
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('.img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao i');
+
+duracaoMusica.textContent = segundospminuto(Math.floor(musica.duration));
+
+// eventos
 document.querySelector('.botao-play').addEventListener('click', tocarmusica);
 
 document.querySelector('.botao-p').addEventListener('click', pausarmusica);
@@ -7,7 +31,7 @@ document.querySelector('.botao-p').addEventListener('click', pausarmusica);
 musica.addEventListener('timeupdate', atualizbarra);
 
 
-// functions
+// funções
 function tocarmusica(){
     musica.play();
     document.querySelector('.botao-p').style.display = 'block';
