c826698ec5c1e651b938bca27066f2a647f52e2d
Luccas Davi <lucctyte@gmail.com>
2022-01-28 12:47:52 -0300

    javascript pt 2
    arrumei tempo decorrido de segundos para minutos

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ document.querySelector('.botao-p').addEventListener('click', pausarmusica);
 
 musica.addEventListener('timeupdate', atualizbarra);
 
+
+// functions
 function tocarmusica(){
     musica.play();
     document.querySelector('.botao-p').style.display = 'block';
@@ -21,4 +23,16 @@ function pausarmusica(){
 function atualizbarra(){
     let barra = document.querySelector('progress');
     barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + "%";
+    let tempocorrido = document.querySelector('.inicio');
+    tempocorrido.textContent = segundospminuto(Math.floor(musica.currentTime));
+}
+
+function segundospminuto(segundos){
+    let campominuto = Math.floor(segundos / 60);
+    let camposegundo = segundos % 60;
+    if(camposegundo < 10){
+        camposegundo = '0' + camposegundo;
+    }
+
+    return campominuto+':'+camposegundo;
 }
